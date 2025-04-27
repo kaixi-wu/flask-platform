8b4426a8df7019784d1b1b0e46c2165c5754eaec
wukaixi <wukx@quarkintl.com>
2025-04-27 12:11:44 +0800

    feat: day_3

--- app/client.py
+++ app/client.py
@@ -27,41 +27,7 @@ def send_message(sock):
 
 
 def main():
-    host = 'customer.saas.idtest.bowenfin.com'
-    port = 80
-    path = '/?loginUserNum=123123&appinfoId=1000'
-
-    try:
-        # Debugging: Print the hostname to check
-        print(f"远程 host: {host}")
-
-        # Resolve the hostname to an IP address
-        ip_address = socket.gethostbyname(host)
-        print(f"host：{host} is {ip_address}")
-
-        # Create a socket object
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # Connect to the server
-        sock.connect((ip_address, port))
-
-        # Send HTTP GET request
-        request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: keep-alive\r\n\r\n"
-        sock.sendall(request.encode())
-
-        # Start a thread to receive messages
-        receive_thread = threading.Thread(target=receive_messages, args=(sock,))
-        receive_thread.start()
-
-        # Main thread to send messages
-        send_message(sock)
-
-    except socket.gaierror as e:
-        print(f"Unable to resolve the server: {e}")
-    except Exception as e:
-        print(f"Unable to connect to the server: {e}")
-    finally:
-        sock.close()
+    pass
 
 
 if __name__ == "__main__":
